6f1de80a63ae19adcbd61d100d96263412f1eb3b
matias <matiaslorenzo466@gmail.com>
2023-01-15 17:24:03 -0300

    feat: new release [1]

--- release.config.js
+++ release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    branches: 'master',
-    repositoryUrl: 'https://github.com/Gauch0/react-app',
+    branches: "master",
+    repositoryUrl: "https://github.com/Gauch0/react-app",
     plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/github',
-    ],
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github",
+    ]
 };
