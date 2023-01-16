dc32ddf73522681c2e526f5ba88264b6ed115205
matias <matiaslorenzo466@gmail.com>
2023-01-16 15:25:45 -0300

    fix package

--- release.config.js
+++ release.config.js
@@ -1,10 +1,9 @@
-module.exports = { 
-    branches: ["master"],
+module.exports = {
+    branches: "master",
     repositoryUrl: "https://github.com/Gauch0/react-app",
     plugins: [
-        '@semantic-release/commit-analyzer', 
-        '@semantic-release/release-notes-generator', 
-        '@semantic-release/github',
-        '@semantic-release/npm',
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/github"
     ]
-}
+  };
