fd2c4996b95cb2353630d17663695787b058715a
matias <matiaslorenzo466@gmail.com>
2023-01-16 15:38:00 -0300

    feat: some feature 2

--- release.config.js
+++ release.config.js
@@ -1,19 +1,10 @@
+
 module.exports = {
     branches: "master",
     repositoryUrl: "https://github.com/Gauch0/react-app",
     plugins: [
-<<<<<<< HEAD
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/github",
-        "@semantic-release/npm",
-        "@semantic-release/git"
-    ]
-};
-=======
       "@semantic-release/commit-analyzer",
       "@semantic-release/release-notes-generator",
       "@semantic-release/github"
     ]
   };
->>>>>>> workflow
